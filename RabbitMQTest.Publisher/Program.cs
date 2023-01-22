ca1f1cbb52e4a5ed49c321b30d14f86abda4d410
sait_ <sait_@DESKTOP-UBEO44F>
2023-01-23 00:08:12 +0300

    Add project files.

---
+++ RabbitMQTest.Publisher/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace UdemyRabbitMQ.publisher
+{
+    public enum LogNames
+    {
+        Successful = 1,
+        Error = 2,
+        Warning = 3,
+        Info = 4,
+    }
+    public enum SoybisType
+    {
+        Arac = 1,
+        Tapu = 2,
+        SGK = 3,
+        EmekliSandiği = 4
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://hlhvpexd:8tyFLTYa1wv1adchaD25UOMlJkS7njzJ@shrimp.rmq.cloudamqp.com/hlhvpexd");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("soybis-direct", durable: true, type: ExchangeType.Direct);
+            var routeKey = $"route-soybis";
+            var queueName = $"direct-queue-soybis";
+            channel.QueueDeclare(queueName, true, false, false);
+            channel.QueueBind(queueName, "soybis-direct", routeKey, null);
+            Enumerable.Range(1, 1000).ToList().ForEach(x =>
+            {
+                int tc = 10000+x;
+                string message = $"{tc}";
+                var messageBody = Encoding.UTF8.GetBytes(message);
+                var routeKey = $"route-soybis";
+                channel.BasicPublish("soybis-direct", routeKey, null, messageBody);
+                Console.WriteLine($"Tc {tc} gönderilmiştir : {message}");
+            });
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ RabbitMQTest.Subscriber/Program.cs
@@ -0,0 +1,78 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace UdemyRabbitMQ.subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://hlhvpexd:8tyFLTYa1wv1adchaD25UOMlJkS7njzJ@shrimp.rmq.cloudamqp.com/hlhvpexd ");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+
+            channel.BasicQos(0, 1, false);
+            var consumer = new EventingBasicConsumer(channel);
+            Console.WriteLine("Logları dinleniyor...");
+            var queueName = $"direct-queue-soybis";
+            channel.BasicConsume(queueName, false, consumer); /*Kuyruklar oluşturuluyor.*/
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var tc = Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine(Arac(tc));
+                Console.WriteLine(Tapu(tc));
+                Console.WriteLine(SGK(tc));
+                Console.WriteLine(EmekliSandiği(tc));
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+            Console.ReadLine();
+        }
+
+        public static string Arac(string tc)
+        {
+            Thread.Sleep(1000);
+            return "Tc Kimlik No Araç soybis yapıldı:" + tc; 
+        }
+        public static string Tapu(string tc)
+        {
+            Thread.Sleep(1000);
+            return "Tapu soybis yapıldı:" + tc;
+        }
+        public static string SGK(string tc)
+        {
+            Thread.Sleep(1000);
+            return "SGK soybis yapıldı:" + tc;
+        }
+        public static string EmekliSandiği(string tc)
+        {
+            Thread.Sleep(1000);
+            return "Emekli Sandığı soybis yapıldı:" + tc;
+        }
+
+
+        public enum LogNames
+        {
+            Successful = 1,
+            Error = 2,
+            Warning = 3,
+            Info = 4,
+        }
+        public enum SoybisType
+        {
+            Arac = 1,
+            Tapu = 2,
+            SGK = 3,
+            EmekliSandiği = 4
+        }
+
+    }
+}
